1fc69799582ef7b39018b264a8d02ee751b2599f
Wes Ferrer <wesferrer@gmail.copm>
2016-12-02 18:43:57 -0800

    finished?

--- js/main.js
+++ js/main.js
@@ -4,6 +4,9 @@ var input, firstNum, op, result;
 var displayEl = document.getElementById('display');
 
 /*--- event listeners ---*/
+document.getElementById('calc')
+ .addEventListener('click', handleClick);
+
 
 
 
@@ -12,15 +15,77 @@ var displayEl = document.getElementById('display');
 /*--- functions ---*/
 
 function initialize () {
-  input = '';
-  firstNum = op = result = null;
-  updateDisplay();
+input = '';
+firstNum = op = result = null;
+updateDisplay();
 }
 
 function updateDisplay(){
-  var text = result || input || '0.'
-  displayEl.textContent = text;
+var text = result || input || '0';
+text += input.includes('.') ? '' : '.';
+displayEl.textContent = text;
+input = '';
+op = firstNum = null;
+}
+
+function handleClick(evt) {
+ var clickedEl = evt.target
+ if (clickedEl.id === 'display') return;
+ switch (clickedEl.textContent) {
+   case 'CA':
+     initialize();
+     break;
+   case '+' :
+     assignOp(add);
+     break;
+   case '←' :
+     input = input.substr(0, input.length -1);
+     break;
+   case '-' :
+     assignOp(sub);
+     break;
+   case '÷' :
+     assignOp(div);
+     break;
+   case '=' :
+     if (!op || !input) return;
+     result = op(parseFloat(firstNum), parseFloat(input));
+     break;
+   case '×' :
+     assignOp(mult);
+     break;
+   case '.' :
+     input += input.includes('.') ? '' : '.';
+     break;
+   default:
+     input += clickedEl.textContent;
+ }
+ updateDisplay();
+}
+
+function assignOp(clickedOp) {
+  if (!input) return;
+  op = clickedOp;
+  firstNum = input;
+  input = '';
 }
 
 
+
 initialize();
+
+/*--- operator functions ---*/
+function add(x, y) {
+  return x + y;
+}
+
+function sub(x, y) {
+  return x - y;
+}
+
+function mult(x, y) {
+  return x * y;
+}
+function div(x, y) {
+  return x / y;
+}
